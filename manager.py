5d75b2fddc049c4af1a6494289ff54b889cf09a1
ChenPi11 <wushengwuxi-msctinoulk@outlook.com>
2023-04-16 20:53:51 +0800

    2023/4/16

--- manager.py
+++ manager.py
@@ -1,8 +1,25 @@
 #!/bin/env python3
-import whiptail
+import whiptail,sys
 from install import *
 from config import *
 
+def main():
+    w=whiptail.Whiptail("交叉编译管理器")
+    res=w.menu("",("安装GNU/Linux交叉编译环境","退出")).decode()
+    print(res)
+    if(res=="退出"):
+        sys.exit(0)
+    elif(res=="安装GNU/Linux交叉编译环境"):
+        gnu_linux_i()
+
 if(__name__=="__main__"):
-    gnu_linux_i()
+    exc=None
+    try:
+        main()
+    except SystemExit:
+        raise
+    except Exception as e:
+        exc=e
     save_conf()
+    if(exc):
+        raise exc
